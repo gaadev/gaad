6e49c1362fe4df9d5524e0f343c3ff69da119981
srillia <srillia@163.com>
2020-12-23 17:36:13 +0800

    git plugin add delete command

--- script/plugin/git/install.sh
+++ script/plugin/git/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 git_download_url_prefix=https://mirrors.edge.kernel.org/pub/software/scm/git/
-git_tar_name=git-2.29.2.tar.xz
+git_tar_xz_name=git-2.29.2.tar.xz
+git_tar_name=git-2.29.2.tar
 git_dir_name=git-2.29.2
 
 function download() {
   wget "$git_download_url_prefix$git_tar_name"
-  xz -d $git_tar_name
+  xz -d $git_tar_xz_name
   tar -xvf $git_tar_name
   cd $git_dir_name
   make
+  rm -rf $git_tar_name
 }
 
 download
